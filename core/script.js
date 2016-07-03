a7aeeaacedd2c3169cbac95726fc9c11bbd8f64b
Batuhan <hello@batuhan.in>
2016-07-03 19:53:39 +0300

    Adding Project
    Version: 1.0.0

---
+++ core/script.js
@@ -0,0 +1,54 @@
+// CORE JAVASCRIPT
+// BY JMDDESIGNS @ CODECANYON
+
+$(document).ready(
+
+function advToggle(){
+        $(".adv_options").hide();
+        $(".advance_toggle").show();
+ 
+    $('.advance_toggle').click(function(){
+    $(".adv_options").slideToggle(300);
+	});
+});
+
+document.getElementById('directionsPanel').onClick();
+function currentYPosition() {
+    if (self.pageYOffset) return self.pageYOffset;
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
+function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+}
+function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+}
